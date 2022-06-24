c07728bd629739c7d38dd9d6121912a845f0abfb
unknown <ac1924705@akfauniversity.org>
2022-06-24 12:59:20 +0500

    custom question widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const QuizApp());
-}
+import './question.dart';
+
+void main() => runApp(const QuizApp());
 
 // class student : public Person{}
 // StatefulWidget vs StatelessWidget
-class QuizApp extends StatelessWidget {
+class QuizApp extends StatefulWidget {
   const QuizApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _QuizAppState();
+  }
+}
+
+class _QuizAppState extends State<QuizApp> {
+  var _tracker = 0;
+  List<String> questions = [
+    'What\'s your favorite color?',
+    'What\'s your favorite animal?',
+    'What\'s your favorite subject?'
+  ];
+
+  void _answerChosen() {
+    setState(() {
+      if (_tracker < questions.length - 1) {
+        _tracker++;
+      } else {
+        _tracker = 0;
+      }
+    });
+    print(_tracker);
+  }
+
+  //QuizApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +43,23 @@ class QuizApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Quiz App'),
         ),
-        body: const Text('Hello!'),
+        body: Column(
+          children: <Widget>[
+            Question(questions[_tracker]),
+            ElevatedButton(
+              onPressed: _answerChosen,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: _answerChosen,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: _answerChosen,
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
